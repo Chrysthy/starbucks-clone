502cc5d152167195cb59981de98d8cd6efd1180d
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-31 21:16:33 -0300

    add bell function

--- src/script/script.js
+++ src/script/script.js
@@ -2,6 +2,9 @@ const buttons = document.querySelectorAll('.menu-button');
 const mainImage = document.querySelector('.cup');
 const circle = document.querySelector('.circle');
 
+const bellButton = document.getElementById('bell-button');
+const bellSound = new Audio('src/audio/bell.mp3');
+
 const dados = [
   { img: 'src/images/img1.png', cor: '#017143' },
   { img: 'src/images/img2.png', cor: '#eb7495' },
@@ -15,3 +18,8 @@ buttons.forEach((button, index) => {
     circle.style.background = dados[index].cor;
   });
 });
+
+bellButton.addEventListener('click', () => {
+  bellSound.play();
+  bellSound.volume = 0.8;
+})
