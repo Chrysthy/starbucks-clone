9443ede7859d3880fca9b066aa37fe3facf96078
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-31 21:07:59 -0300

    update javascript

--- src/script/script.js
+++ src/script/script.js
@@ -1,31 +1,17 @@
-const cups = document.querySelectorAll('.menu-button');
+const buttons = document.querySelectorAll('.menu-button');
 const mainImage = document.querySelector('.cup');
 const circle = document.querySelector('.circle');
 
-
 const dados = [
-  { img: '../images/img1.png', cor: '#017143' },
-  { img: '../images/img2.png', cor: '#eb7495' },
-  { img: '../images/img3.png', cor: '#d752b1' }
+  { img: 'src/images/img1.png', cor: '#017143' },
+  { img: 'src/images/img2.png', cor: '#eb7495' },
+  { img: 'src/images/img3.png', cor: '#d752b1' }
 ];
 
-
-document.querySelectorAll('.menu-button').forEach((button, index) => {
-
-    button.addEventListener('click', function (event) {
-
-        event.preventDefault(); 
-
-        cups.forEach((cup, index) => {
-        
-          cup.addEventListener('click', () => {
-        
-            mainImage.src = dados[index].img;
-            circle.style.background = dados[index].cor;
-          });
-        });
-
-
-    });
+buttons.forEach((button, index) => {
+  button.addEventListener('click', (event) => {
+    event.preventDefault();
+    mainImage.src = dados[index].img;
+    circle.style.background = dados[index].cor;
+  });
 });
-
